67b55d69ba535f9f65be32097ee04224b2e4d2f4
Zoubeir-Hm <zoubeirhamouche@gmail.com>
2024-10-13 03:49:32 +0100

    add comment to aliases.sh

--- config/aliases.sh
+++ config/aliases.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# Alias courants pour les outils du projet
+# Start a development server for a web project (example with Python)
+alias startserver='. ~/bash-scripts-project/scripts/dev-tools/start_server.sh'
 # Source the mkcd script directly to use it in the current shell session
 alias mkcd='. ~/bash-scripts-project/scripts/file-management/mkcd.sh'
+# Function to extract different types of archive files
 alias extract='. ~/bash-scripts-project/scripts/file-management/extract.sh'
+# Clean the system by removing temporary files and cache
 alias cleanup='. ~/bash-scripts-project/scripts/system-management/cleanup.sh'
+# Test network connectivity with a given site
 alias pingtest='. ~/bash-scripts-project/scripts/network-tools/ping_test.sh'
 
