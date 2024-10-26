b63fe7407afb57fb57a31b269d5ebbe1572a93ae
Zoubeir-Hm <zoubeirhamouche@gmail.com>
2024-10-26 23:09:51 +0100

    update file aliases.sh

--- config/aliases.sh
+++ config/aliases.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-
+PROJECT_DIR=$(pwd)
 
 #-------------------------file-management
 # Source the mkcd script directly to use it in the current shell session
-alias mkcd='. ~/bash-scripts-project/scripts/file-management/mkcd.sh'
+alias mkcd=". $PROJECT_DIR/scripts/file-management/mkcd.sh"
 # Function to extract different types of archive files
-alias extract='. ~/bash-scripts-project/scripts/file-management/extract.sh'
+alias extract=". $PROJECT_DIR/scripts/file-management/extract.sh"
 # Function to navigate up multiple directories
-alias ccd='. ~/bash-scripts-project/scripts/file-management/ccd.sh'
+alias ccd=". $PROJECT_DIR/scripts/file-management/ccd.sh"
 
 
 #-------------------------dev-tools
 # Start a development server for a web project (example with Python)
-alias startserver='. ~/bash-scripts-project/scripts/dev-tools/start_server.sh'
+alias startserver=". $PROJECT_DIR/scripts/dev-tools/start_server.sh"
 
 
 #-------------------------system-management
 # Clean the system by removing temporary files and cache
-alias cleanup='. ~/bash-scripts-project/scripts/system-management/cleanup.sh'
+alias cleanup=". $PROJECT_DIR/scripts/system-management/cleanup.sh"
 # Function to check if a command is installed
-alias command_check='. ~/bash-scripts-project/scripts/system-management/command_check.sh'
+alias command_check=". $PROJECT_DIR/scripts/system-management/command_check.sh"
 
 #-------------------------network-tools
 # Test network connectivity with a given site
-alias pingtest='. ~/bash-scripts-project/scripts/network-tools/ping_test.sh'
+alias pingtest=". $PROJECT_DIR/scripts/network-tools/ping_test.sh"
 
 
